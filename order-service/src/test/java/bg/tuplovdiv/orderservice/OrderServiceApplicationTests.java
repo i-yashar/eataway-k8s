0fbc8f09aca18e75211b8227a767e6819bdf0200
Ibrahim Yashar <ibo7777@abv.bg>
2023-03-08 19:45:55 +0200

    order service creation

---
+++ order-service/src/test/java/bg/tuplovdiv/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.tuplovdiv.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
