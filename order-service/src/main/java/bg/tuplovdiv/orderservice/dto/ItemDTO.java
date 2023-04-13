a80f95a42bdb6c4e7660197c4c559814aef7f13b
Ibrahim Yashar <ibo7777@abv.bg>
2023-04-13 20:16:08 +0300

    extract additional DTOs

---
+++ order-service/src/main/java/bg/tuplovdiv/orderservice/dto/ItemDTO.java
@@ -0,0 +1,37 @@
+package bg.tuplovdiv.orderservice.dto;
+
+import java.util.UUID;
+
+public class ItemDTO {
+
+    private UUID itemId;
+    private String name;
+    private Integer quantity;
+
+    public UUID getItemId() {
+        return itemId;
+    }
+
+    public ItemDTO setItemId(UUID itemId) {
+        this.itemId = itemId;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ItemDTO setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public ItemDTO setQuantity(Integer quantity) {
+        this.quantity = quantity;
+        return this;
+    }
+}
