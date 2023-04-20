9affb6869aaf64437e28dde268f693753d06114a
Ibrahim Yashar <ibo7777@abv.bg>
2023-04-20 21:00:29 +0300

    add basket item endpoint

--- order-service/src/main/java/bg/tuplovdiv/orderservice/rest/BasketRestController.java
+++ order-service/src/main/java/bg/tuplovdiv/orderservice/rest/BasketRestController.java
@@ -2,6 +2,7 @@ package bg.tuplovdiv.orderservice.rest;
 
 import bg.tuplovdiv.orderservice.dto.BasketDTO;
 import bg.tuplovdiv.orderservice.dto.BasketItemDTO;
+import bg.tuplovdiv.orderservice.service.BasketService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,10 +15,16 @@ public class BasketRestController {
     private static final String BASKET_PATH = "/users/{ownerId}/basket";
     private static final String BASKET_ITEM_PATH = BASKET_PATH + "/items/{menuId}";
 
+    private final BasketService basketService;
+
+    public BasketRestController(BasketService basketService) {
+        this.basketService = basketService;
+    }
+
     @PutMapping(BASKET_PATH)
-    public ResponseEntity<Void> addBasketItem(@PathVariable UUID ownerId,
+    public ResponseEntity<BasketDTO> addBasketItem(@PathVariable UUID ownerId,
                                               @RequestBody BasketItemDTO basketItem) {
-        return null;
+        return ResponseEntity.ok(basketService.addBasketItem(ownerId, basketItem));
     }
 
     @GetMapping(BASKET_PATH)
