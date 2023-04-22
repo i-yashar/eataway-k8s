354f0801059305fee48efb59491f85e83727c0e1
Ibrahim Yashar <ibo7777@abv.bg>
2023-04-22 13:04:30 +0300

    order controller initial implementation

--- api-gateway/src/main/java/bg/tuplovdiv/apigateway/controller/OrderController.java
+++ api-gateway/src/main/java/bg/tuplovdiv/apigateway/controller/OrderController.java
@@ -1,8 +1,17 @@
 package bg.tuplovdiv.apigateway.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("eataway")
 public class OrderController {
 
+    private static final String BASKET_PATH = "basket";
+
+    @GetMapping(BASKET_PATH)
+    public String getUserBasket() {
+        return "basket";
+    }
 }
