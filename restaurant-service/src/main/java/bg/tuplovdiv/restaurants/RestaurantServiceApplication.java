fbd6486d005098d39255ab38962710bb47e20e89
Ibrahim Yashar <ibo7777@abv.bg>
2023-05-20 20:08:52 +0300

    implement service to service security

--- restaurant-service/src/main/java/bg/tuplovdiv/restaurants/RestaurantServiceApplication.java
+++ restaurant-service/src/main/java/bg/tuplovdiv/restaurants/RestaurantServiceApplication.java
@@ -1,9 +1,12 @@
 package bg.tuplovdiv.restaurants;
 
+import bg.tuplovdiv.restaurants.config.security.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class RestaurantServiceApplication {
 
     public static void main(String[] args) {
--- restaurant-service/src/main/java/bg/tuplovdiv/restaurants/config/security/SecurityConfig.java
+++ restaurant-service/src/main/java/bg/tuplovdiv/restaurants/config/security/SecurityConfig.java
@@ -3,22 +3,42 @@ package bg.tuplovdiv.restaurants.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final RsaKeyProperties rsaKeys;
+
+    public SecurityConfig(RsaKeyProperties rsaKeys) {
+        this.rsaKeys = rsaKeys;
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+    }
 
     @Bean
     public SecurityFilterChain httpSecurity(HttpSecurity http) throws Exception {
+        // @formatter:off
         http
                 .csrf().disable()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+            .and()
                 .authorizeHttpRequests()
                 .anyRequest()
-                .permitAll();
+                .authenticated()
+            .and()
+                .oauth2ResourceServer()
+                .jwt();
+        // @formatter:on
 
         return http.build();
     }
