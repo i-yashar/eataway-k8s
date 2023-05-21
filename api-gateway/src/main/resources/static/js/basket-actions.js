7a1829de697bb503224fceb20cc6d5ef83265bc2
Ibrahim Yashar <ibo7777@abv.bg>
2023-05-21 11:23:06 +0300

    small ui fix

--- api-gateway/src/main/resources/static/js/basket-actions.js
+++ api-gateway/src/main/resources/static/js/basket-actions.js
@@ -1,8 +1,14 @@
 let addMenuToBasketButtons = Array.from(document.getElementsByClassName('addMenuToBasketBtn'))
 let removeMenuFromBasketButtons = Array.from(document.getElementsByClassName('removeMenuFromBasketBtn'))
+let totalCost = document.getElementById('totalCostButton')
+let orderForm = document.getElementById('orderForm')
 
 addMenuToBasketButtons.forEach(button => button.addEventListener('click', onAddMenuToBasket))
 removeMenuFromBasketButtons.forEach(button => button.addEventListener('click', onRemoveMenuFromBasket))
+
+if(totalCost.textContent === 'Total: 0.00$') {
+    orderForm.style.visibility = 'hidden'
+}
 
 async function onAddMenuToBasket(event) {
     event.preventDefault()
