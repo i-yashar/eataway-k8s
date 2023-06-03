76587eaf3224a505c6625e50f6e84f2e88b18aa1
Ibrahim Yashar <ibo7777@abv.bg>
2023-06-03 16:38:25 +0300

    add delivery driver not free exception handler

--- api-gateway/src/main/java/bg/tuplovdiv/apigateway/security/validation/DeliveryValidator.java
+++ api-gateway/src/main/java/bg/tuplovdiv/apigateway/security/validation/DeliveryValidator.java
@@ -1,5 +1,6 @@
 package bg.tuplovdiv.apigateway.security.validation;
 
+import bg.tuplovdiv.apigateway.exception.DeliveryDriverNotFreeException;
 import bg.tuplovdiv.apigateway.model.entity.DeliveryDriverEntity;
 import bg.tuplovdiv.apigateway.repository.DeliveryDriverRepository;
 import bg.tuplovdiv.apigateway.security.authentication.AuthenticatedUserProvider;
@@ -31,7 +32,11 @@ public class DeliveryValidator {
             return false;
         }
 
-        return driver.get().isFree();
+        if(!driver.get().isFree()) {
+            throw new DeliveryDriverNotFreeException();
+        }
+
+        return true;
     }
 
     public boolean isDeliveryDriverCorrect(UUID orderId) {
