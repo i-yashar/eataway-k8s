efe428ea91b02b11cb2cc82a0e8b44b7d435c946
Ibrahim Yashar <ibo7777@abv.bg>
2023-06-09 20:38:39 +0300

    remove redundant classes

--- order-service/src/main/java/bg/tuplovdiv/orderservice/messaging/impl/OrderListener.java
+++ order-service/src/main/java/bg/tuplovdiv/orderservice/messaging/impl/OrderListener.java
@@ -1,19 +1,26 @@
 package bg.tuplovdiv.orderservice.messaging.impl;
 
+import bg.tuplovdiv.orderservice.dto.CreateOrderRequestDTO;
 import bg.tuplovdiv.orderservice.dto.OrderDTO;
 import bg.tuplovdiv.orderservice.service.OrderService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import static bg.tuplovdiv.orderservice.config.RabbitMQConfig.ORDER_CREATE_QUEUE;
 import static bg.tuplovdiv.orderservice.config.RabbitMQConfig.ORDER_UPDATE_QUEUE;
 
 @Component
-class OrderUpdateListener {
+class OrderListener {
 
     private final OrderService orderService;
 
-    OrderUpdateListener(OrderService orderService) {
+    OrderListener(OrderService orderService) {
         this.orderService = orderService;
+    }
+
+    @RabbitListener(queues = ORDER_CREATE_QUEUE)
+    public void accept(CreateOrderRequestDTO orderRequest) {
+        orderService.createOrder(orderRequest);
     }
 
     @RabbitListener(queues = ORDER_UPDATE_QUEUE)
